e9e91e672ade9acb95dce0501da5ca6b88a042ef
Isabel Jenkins <issyjenk@gmail.com>
2018-10-16 04:20:54 +0100

    Added keras code. Shape of input data currently doesn't fit NN input. Needs to be worked on

--- step2.py
+++ step2.py
@@ -2,7 +2,24 @@
 import os
 import re
 import pickle
-from random import shuffle
+import random
+
+def chunk(deck):
+    processed_x = []
+    processed_y = []
+
+    for idx in range(0, len(deck) - 1):
+        slot_x = [-1] * 74
+        slot_y = [0] * len(all_cards)
+
+        for idx2 in range(0, idx + 1):
+            slot_x[idx2] = deck[idx2]
+
+        processed_x.append(slot_x)
+        slot_y[deck[idx + 1]] = 1
+        processed_y.append(slot_y)
+
+    return processed_x, processed_y
 
 # The path to our deck files
 decks_path = './decks'
@@ -10,7 +27,8 @@ decks_path = './decks'
 # Lists for storing results
 deck_lists = []
 all_cards = []
-processed_deck_lists = []
+processed_deck_lists_x = []
+processed_deck_lists_y = []
 
 # Regex for scraping the number of each card in a deck
 num_finder = re.compile('[0-9]*')
@@ -55,15 +73,23 @@ for deck_list in deck_lists:
     # Ignore decks that are not 75 cards in size (60 mainboard + 15 sideboard)
     # This makes life way easier with the LSTM
     if len(processed_deck_list) == 75:
-        processed_deck_lists.append(processed_deck_list)
+        x, y = chunk(processed_deck_list)
+        processed_deck_lists_x.append(x)
+        processed_deck_lists_y.append(y)
         # Create 9 Shuffled versions of the decks as well
         for i in range(0, 9):
-            processed_deck_lists.append(shuffle(processed_deck_list))
+            x, y = chunk(random.sample(processed_deck_list, len(processed_deck_list)))
+            processed_deck_lists_x.append(x)
+            processed_deck_lists_y.append(y)
 
 # Save the all_cards list for later use
 with open('cards_lists.pkl', 'wb') as fp:
     pickle.dump(all_cards, fp)
 
-# Save the processed_deck_lists for later use
-with open('deck_lists.pkl', 'wb') as fp:
-    pickle.dump(processed_deck_lists, fp)
+# Save the processed_deck_lists_x for later use
+with open('deck_lists_x.pkl', 'wb') as fp:
+    pickle.dump(processed_deck_lists_x, fp)
+
+# Save the processed_deck_lists_y for later use
+with open('deck_lists_y.pkl', 'wb') as fp:
+    pickle.dump(processed_deck_lists_y, fp)
---
+++ step3.py
@@ -0,0 +1,52 @@
+import numpy as np
+import pickle
+import os
+import shutil
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Activation, LSTM, Flatten
+from keras.callbacks import ModelCheckpoint
+from keras.utils import np_utils
+
+dataX = []
+dataY = []
+cards = []
+
+with open('deck_lists_x.pkl','rb') as f:
+    dataX = np.asarray(pickle.load(f))
+
+with open('deck_lists_y.pkl','rb') as f:
+    dataY = np.asarray(pickle.load(f))
+
+with open('cards_lists.pkl','rb') as f:
+    cards = pickle.load(f)
+
+X = np.reshape(dataX, (1, 74, len(dataX) * 74))
+Y = dataY
+
+# define the LSTM model
+model = Sequential()
+# model.add(Embedding((len(cards) + 1), 128, input_length=74))
+model.add(LSTM(256, input_shape=(None, 74)))
+
+# Softmax
+model.add(Dense(len(cards), init='uniform'))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
+# Delete the existing decks dir
+models_path = './models'
+
+if os.path.isdir(models_path):
+    shutil.rmtree(models_path)
+
+# Create new decks dir
+os.makedirs(models_path)
+
+# define the checkpoint
+filepath = models_path + "/weights-improvement-{epoch:02d}-{loss:.4f}.hdf5"
+checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=1, save_best_only=True, mode='min')
+callbacks_list = [checkpoint]
+
+# fit the model
+model.fit(X, Y, epochs=20, callbacks=callbacks_list, verbose=1)
